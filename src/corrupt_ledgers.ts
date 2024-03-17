9cf226474c20e2456f0faef30e85c8a682718d4a
Ankan <ankan.anurag@gmail.com>
2024-03-17 23:52:18 +0100

    fix corrupt ledger search

--- src/corrupt_ledgers.ts
+++ src/corrupt_ledgers.ts
@@ -19,9 +19,6 @@ async function main() {
 	const options = await optionsPromise;
 	const provider = new WsProvider(options.endpoint);
 	const api = await ApiPromise.create({ provider });
-	const latest = await api.derive.chain.bestNumber();
-	const latest_hash = await api.rpc.chain.getBlockHash(latest);
-	const apiAt = await api.at(latest_hash);
 
 	console.log(
 		`****************** Connected to node: ${(await api.rpc.system.chain()).toHuman()} [ss58: ${
@@ -29,66 +26,73 @@ async function main() {
 		}] ******************`
 	);
 
-	const controllers = await corrupt_ledgers(apiAt);
-	// iterate on set
-	for (const controller of controllers) {
-		await when_controller_deprecated(api, controller);
-	}
+	await corrupt_ledgers(api);
 
 	process.exit(0);
 }
 
-async function corrupt_ledgers(apiAt: ApiDecoration<'promise'>) {
+// find bonded accounts where controller not == stash and associated ledger. If
+// associated ledger is none or ledger.stash not equal to our stash, then its
+// corrupt.
+async function corrupt_ledgers(api: ApiPromise) {
+	const latest = await api.derive.chain.bestNumber();
+	const latest_hash = await api.rpc.chain.getBlockHash(latest);
+	const apiAt = await api.at(latest_hash);
+
 	const validators = await get_all_validators(apiAt);
-	const reverse_bonded = new Map(); // controller -> stash
-	const duplicate_controllers = new Set<string>();
+	const all_ledgers = await get_all_ledgers(apiAt);
 
-	(await apiAt.query.staking.bonded.entries()).map(([stash, controller]) => {
-		if (reverse_bonded.has(controller.toHuman())) {
-			const stash_two = reverse_bonded.get(controller.toHuman());
-			console.log(
-				'\x1b[36m%s\x1b[0m',
-				`🙈 🙉 🙊 Duplicate controller found: ${controller.toHuman()} | stash 1: ${stash.toHuman()} | stash 2: ${stash_two}`
+	for (const [stash, controller] of await apiAt.query.staking.bonded.entries()) {
+		// console.log(`Checking: ${controller.toHuman()} | ${stash.toHuman()}`);
+		const ledger = all_ledgers.get(controller.toHuman()?.toString());
+
+		if (
+			ledger == undefined ||
+			ledger.isNone ||
+			ledger.unwrap().stash.toHuman() != stash.toHuman()
+		) {
+			// console.log(
+			// 	'\n\n\x1b[36m%s\x1b[0m',
+			// 	`🙈 🙉 🙊 Corrupt ledger found for controller: ${controller.toHuman()} | stash: ${stash.toHuman()}`
+			// );
+			//
+			// console.log(
+			// 	'\x1b[31m%s\x1b[0m',
+			// 	`⚙️  ⚙️  ⚙️  is_validator controller: ${validators.includes(
+			// 		controller.toHuman()?.toString()
+			// 	)} | stash ${validators.includes(stash.toHuman()?.toString())}`
+			// );
+
+			await when_ledger_corruption(
+				api,
+				stash.toHuman()?.toString() ?? '',
+				controller.toHuman()?.toString() ?? ''
 			);
-
-			console.log(
-				'\x1b[31m%s\x1b[0m',
-				`⚙️  ⚙️  ⚙️  is_validator controller: ${validators.includes(
-					controller.toHuman()?.toString()
-				)} | stash1 ${validators.includes(
-					stash.toHuman()?.toString()
-				)} | stash 2: ${validators.includes(stash_two.toString())}`
-			);
-
-			duplicate_controllers.add(controller.toString());
-		} else {
-			reverse_bonded.set(controller.toHuman(), stash.toHuman());
 		}
-	});
-
-	return duplicate_controllers;
+	}
 }
 
 async function get_all_validators(apiAt: ApiDecoration<'promise'>) {
 	return (await apiAt.query.staking.validators.keys()).map((key) => key.toHuman()?.toString());
 }
 
-/// checks when controller and stash became same.
-async function when_controller_deprecated(api: ApiPromise, bonded_account: string) {
+// return map of controller -> ledger
+async function get_all_ledgers(apiAt: ApiDecoration<'promise'>) {
+	const map = new Map();
+	(await apiAt.query.staking.ledger.entries()).map(([key, val]) =>
+		map.set(key.toHuman()?.toString(), val)
+	);
+
+	return map;
+}
+
+/// checks when ledger became corrupt.
+async function when_ledger_corruption(api: ApiPromise, stash_str: string, controller_str: string) {
 	const latest = await api.derive.chain.bestNumber();
 	const latest_hash = await api.rpc.chain.getBlockHash(latest);
 	const apiAt = await api.at(latest_hash);
 
-	const acct = apiAt.registry.createType('AccountId', bonded_account);
-	const latest_val = await apiAt.query.staking.bonded(acct);
-
-	if (latest_val.isNone || latest_val.unwrap().toHuman() != acct.toHuman()) {
-		console.log(`❌ ❌ ❌: Controller ${bonded_account} not deprecated yet.`);
-		return;
-	}
-
-	// console.log(`Binary searching change in bonded value for: ${bonded_account}`);
-	// console.log(`Current value: ${latest_val.toHuman()}`);
+	const controller = apiAt.registry.createType('AccountId', controller_str);
 
 	let start = 0;
 	let end = latest.toNumber();
@@ -99,22 +103,35 @@ async function when_controller_deprecated(api: ApiPromise, bonded_account: strin
 
 		const blockHash = await api.rpc.chain.getBlockHash(mid);
 		const apiAt = await api.at(blockHash);
-		const bonded = await apiAt.query.staking.bonded(acct);
+		const ledger = await apiAt.query.staking.ledger(controller);
 		// console.log(`bonded exists: ${bonded.isSome} at block: ${mid}`);
 
-		if (bonded.toHuman() == latest_val.toHuman()) {
+		if (
+			ledger != undefined &&
+			ledger.isSome &&
+			ledger.unwrap().stash.toHuman().toString() != stash_str
+		) {
+			// corruption already has happened, go further in past
 			end = mid - 1;
 		} else {
 			start = mid + 1;
 		}
 	}
 
-	console.log(
-		'\x1b[36m%s\x1b[0m',
-		`🎭 🆘 ⚠️  🚧 Ledger overwritten for Controller ${bonded_account} at block: ${start} with hash: ${await api.rpc.chain.getBlockHash(
-			start
-		)}`
-	);
+	if (start >= latest.toNumber()) {
+		// could not find result
+		console.log(
+			'\x1b[31m%s\x1b[0m',
+			`🙈 🙉 🙊 Could not find when ledger corrupted for controller: ${controller_str} | stash: ${stash_str}`
+		);
+	} else {
+		console.log(
+			'\x1b[36m%s\x1b[0m',
+			`🎭 🆘 ⚠️  🚧 Ledger overwritten for controller: ${controller_str} | stash: ${stash_str} at block: ${start} with hash: ${await api.rpc.chain.getBlockHash(
+				start
+			)}`
+		);
+	}
 }
 
 main().catch(console.error);
