175942cfbe7827e730b1640be0b5ddc2df937114
Ankan <ankan.anurag@gmail.com>
2025-05-17 20:42:26 +0200

    add more pallets

--- src/ahm-compare.ts
+++ src/ahm-compare.ts
@@ -9,18 +9,9 @@ import { Balance } from '@polkadot/types/interfaces/runtime';
 import yargs from 'yargs';
 import { hideBin } from 'yargs/helpers';
 
-const optionsPromise = yargs(hideBin(process.argv)).option('endpoint', {
-	alias: 'e',
-	type: 'string',
-	default: 'wss://polkadot-rpc.dwellir.com',
-	description: 'the wss endpoint. It must allow unsafe RPCs.',
-	required: true
-}).argv;
-
-// Westend Block: `0x0dea21fe90fe4fd5cb69e76f9e9acefbf1e5fb20bc22160c230f2626be70efcf`
-
 async function main() {
-	const westend_endpoint = 'wss://westend-rpc.dwellir.com';
+	// const westend_endpoint = 'wss://westend-rpc.dwellir.com';
+	const westend_endpoint = 'wss://westend-rpc-tn.dwellir.com';
 	const westend_provider = new WsProvider(westend_endpoint);
 	const westend_api = await ApiPromise.create({ provider: westend_provider });
 	const westend_block = 26041702;
@@ -30,27 +21,34 @@ async function main() {
 	const wah_endpoint = 'wss://asset-hub-westend-rpc.dwellir.com';
 	const wah_provider = new WsProvider(wah_endpoint);
 	const wah_api = await ApiPromise.create({ provider: wah_provider });
-	const wah_block = 11736597;
+	const wah_block = 11736597; // after second migration ended
+	// const wah_block = 11736080; // before second migration started
 	const wah_hash = await wah_api.rpc.chain.getBlockHash(wah_block);
 	const wah_apiAt = await wah_api.at(wah_hash);
 
-	console.log(
-		`****************** Connected to node 01: ${(
-			await westend_api.rpc.system.chain()
-		).toHuman()} [ss58: ${westend_api.registry.chainSS58}] ******************`
-	);
+	console.log(`*****Reading AH state at block: ${wah_block}******`);
 
-	console.log(
-		`****************** Connected to node 02: ${(
-			await wah_api.rpc.system.chain()
-		).toHuman()} [ss58: ${wah_api.registry.chainSS58}] ******************`
-	);
+	const pallets = [
+		'staking',
+		'nominationPools',
+		'fastUnstake',
+		'treasury',
+		'multisig',
+		'proxy',
+		'preimage',
+		'vesting',
+		'indices',
+		'referenda',
+		'scheduler',
+		'convictionVoting',
+		// 'bounties',
+		'assetRate'
+	];
 
-	const pallets = ['staking', 'nominationPools', 'fastUnstake', 'treasury'];
-
-	console.log(`## Westend\n`);
+	/*
+	console.log(`\n## Westend\n`);
 	for (const pallet of pallets) {
-		console.log(`### ${pallet}\n`);
+		console.log(`\n### ${pallet}\n`);
 		const westend_palletFn = (westend_apiAt.query as any)[pallet];
 
 		for (const [name, _item] of Object.entries(westend_palletFn)) {
@@ -71,8 +69,7 @@ async function main() {
 			}
 		}
 	}
-
-	console.log(`---\n`);
+*/
 	console.log(`## Asset Hub`);
 
 	for (const pallet of pallets) {
@@ -89,7 +86,7 @@ async function main() {
 						console.log(`${name}: ${keys.length} items`);
 					} else {
 						const value = await storageFn();
-						console.log(`${name}:`, value.toHuman());
+						console.log(`StorageValue ${name}:`, value.toHuman());
 					}
 				} catch (err) {
 					console.warn(`Failed to query ${name}:`, err);
