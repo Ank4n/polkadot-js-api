a6bfb620cac7bf6a6c5d97a8c33546c6bdff5b5f
Ankan <ankan.anurag@gmail.com>
2025-05-18 10:48:09 +0200

    update to generate diff

--- src/ahm-compare.ts
+++ src/ahm-compare.ts
@@ -10,23 +10,25 @@ import yargs from 'yargs';
 import { hideBin } from 'yargs/helpers';
 
 async function main() {
-	// const westend_endpoint = 'wss://westend-rpc.dwellir.com';
-	const westend_endpoint = 'wss://westend-rpc-tn.dwellir.com';
-	const westend_provider = new WsProvider(westend_endpoint);
-	const westend_api = await ApiPromise.create({ provider: westend_provider });
-	const westend_block = 26041702;
-	const westend_block_hash = await westend_api.rpc.chain.getBlockHash(westend_block);
-	const westend_apiAt = await westend_api.at(westend_block_hash);
+	// const one_endpoint = 'wss://westend-rpc.dwellir.com';
+	// const one_endpoint = 'wss://westend-rpc-tn.dwellir.com';
+	// const one_endpoint = 'wss://asset-hub-westend-rpc.dwellir.com';
+	const one_endpoint = 'wss://westmint-rpc-tn.dwellir.com';
+	const one_provider = new WsProvider(one_endpoint);
+	const one_api = await ApiPromise.create({ provider: one_provider });
+	// const one_block = 26041702; // westend before first migration
+	const one_block = 11736080; // wah before second migration started
+	const one_block_hash = await one_api.rpc.chain.getBlockHash(one_block);
+	const one_apiAt = await one_api.at(one_block_hash);
 
-	const wah_endpoint = 'wss://asset-hub-westend-rpc.dwellir.com';
-	const wah_provider = new WsProvider(wah_endpoint);
-	const wah_api = await ApiPromise.create({ provider: wah_provider });
-	const wah_block = 11736597; // after second migration ended
-	// const wah_block = 11736080; // before second migration started
-	const wah_hash = await wah_api.rpc.chain.getBlockHash(wah_block);
-	const wah_apiAt = await wah_api.at(wah_hash);
-
-	console.log(`*****Reading AH state at block: ${wah_block}******`);
+	// const two_endpoint = 'wss://asset-hub-westend-rpc.dwellir.com';
+	const two_endpoint = 'wss://westmint-rpc-tn.dwellir.com';
+	const two_provider = new WsProvider(two_endpoint);
+	const two_api = await ApiPromise.create({ provider: two_provider });
+	const two_block = 11736597; // wah after second migration ended
+	// const two_block = 11736080; // wah before second migration started
+	const two_hash = await two_api.rpc.chain.getBlockHash(two_block);
+	const two_apiAt = await two_api.at(two_hash);
 
 	const pallets = [
 		'staking',
@@ -45,48 +47,30 @@ async function main() {
 		'assetRate'
 	];
 
-	/*
-	console.log(`\n## Westend\n`);
-	for (const pallet of pallets) {
-		console.log(`\n### ${pallet}\n`);
-		const westend_palletFn = (westend_apiAt.query as any)[pallet];
+	const map = new Map<string, string>();
 
-		for (const [name, _item] of Object.entries(westend_palletFn)) {
-			const storageFn = (westend_palletFn as any)[name];
+	console.log(
+		`Going through pallets in Node 01: ${(
+			await one_api.rpc.system.chain()
+		).toHuman()} at block ${one_block}`
+	);
+
+	for (const pallet of pallets) {
+		console.log(`Pallet: ${pallet}`);
+		const one_palletFn = (one_apiAt.query as any)[pallet];
+
+		for (const [name, _item] of Object.entries(one_palletFn)) {
+			const storageFn = (one_palletFn as any)[name];
 
 			if (typeof storageFn === 'function') {
 				try {
 					if ('keys' in storageFn && typeof storageFn.keys === 'function') {
 						const keys = await storageFn.keys();
-						console.log(`${name}: ${keys.length} items`);
+						map.set(pallet + '::' + name + ' map keys', keys.length);
+						// console.log(`${name}: ${keys.length} items`);
 					} else {
 						const value = await storageFn();
-						console.log(`${name}:`, value.toHuman());
-					}
-				} catch (err) {
-					console.warn(`Failed to query ${name}:`, err);
-				}
-			}
-		}
-	}
-*/
-	console.log(`## Asset Hub`);
-
-	for (const pallet of pallets) {
-		console.log(`### ${pallet}`);
-		const wah_palletFn = (wah_apiAt.query as any)[pallet];
-
-		for (const [name, _item] of Object.entries(wah_palletFn)) {
-			const storageFn = (wah_palletFn as any)[name];
-
-			if (typeof storageFn === 'function') {
-				try {
-					if ('keys' in storageFn && typeof storageFn.keys === 'function') {
-						const keys = await storageFn.keys();
-						console.log(`${name}: ${keys.length} items`);
-					} else {
-						const value = await storageFn();
-						console.log(`StorageValue ${name}:`, value.toHuman());
+						map.set(pallet + '::' + name + ' value', value.toHuman());
 					}
 				} catch (err) {
 					console.warn(`Failed to query ${name}:`, err);
@@ -95,6 +79,51 @@ async function main() {
 		}
 	}
 
+	console.log(
+		`Going through pallets in Node 02: ${(
+			await two_api.rpc.system.chain()
+		).toHuman()} at block ${two_block}`
+	);
+
+	console.log('Generating Diff');
+
+	for (const pallet of pallets) {
+		console.log(`\n====== Pallet: ${pallet} =======\n`);
+		const two_palletFn = (two_apiAt.query as any)[pallet];
+
+		for (const [name, _item] of Object.entries(two_palletFn)) {
+			const storageFn = (two_palletFn as any)[name];
+
+			if (typeof storageFn === 'function') {
+				try {
+					if ('keys' in storageFn && typeof storageFn.keys === 'function') {
+						const keys = await storageFn.keys();
+						// compare against node 01 storage.
+						const map_key = pallet + '::' + name + ' map keys';
+						const first = map.get(map_key);
+						const second = keys.length;
+						if (first !== second) {
+							console.log(`⚠️ ⚠️ ${map_key}: value changed from <${first}> to <${second}>`);
+						}
+						// console.log(`${name}: ${keys.length} items`);
+					} else {
+						const value = await storageFn();
+						const map_key = pallet + '::' + name + ' value';
+						const first = map.get(map_key);
+						const second = value.toHuman();
+						if (first !== second) {
+							console.log(`⚠️ ⚠️ ${map_key}: value changed from <${first}> to <${second}>`);
+						}
+						// console.log(`StorageValue ${name}:`, value.toHuman());
+					}
+				} catch (err) {
+					console.warn(`Failed to query ${name}:`, err);
+				}
+			}
+		}
+	}
+
+	console.log('----------------------------------');
 	process.exit(0);
 }
 
